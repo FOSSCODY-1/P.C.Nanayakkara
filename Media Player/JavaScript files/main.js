7951903a3f4f662dc4228ebef0b6281f52f7f72f
PrabhaniN <40514527+PrabhaniN@users.noreply.github.com>
2018-09-24 19:42:00 +0530

    JS updated in dropdown

---
+++ Media Player/JavaScript files/main.js
@@ -0,0 +1,14 @@
+var video = document.getElementsByClassName("video_button");
+var i;
+
+for (i = 0; i < video.length; i++) {
+    video[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var videocontent = this.nextElementSibling;
+        if (videocontent.style.display === "block") {
+            videocontent.style.display = "none";
+        } else {
+            videocontent.style.display = "block";
+        }
+    });
+}
