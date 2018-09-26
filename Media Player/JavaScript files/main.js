570a0031bf24801687496f4d3960edaac62a6718
PrabhaniN <40514527+PrabhaniN@users.noreply.github.com>
2018-09-27 00:49:06 +0530

    title bar icon, css ref link and more js part

--- Media Player/JavaScript files/main.js
+++ Media Player/JavaScript files/main.js
@@ -5,10 +5,10 @@ for (i = 0; i < video.length; i++) {
     video[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var videocontent = this.nextElementSibling;
-        if (videocontent.style.display === "block") {
-            videocontent.style.display = "none";
+        if (videocontent.stylesheet.display === "block") {
+            videocontent.stylesheet.display = "none";
         } else {
-            videocontent.style.display = "block";
+            videocontent.stylesheet.display = "block";
         }
     });
 }
@@ -16,14 +16,14 @@ for (i = 0; i < video.length; i++) {
 var audio = document.getElementsByClassName("audio_button");
 var i;
 
-for (i = 0; i < video.length; i++) {
+for (i = 0; i < audio.length; i++) {
     audio[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var audiocontent = this.nextElementSibling;
-        if (audiocontent.style.display === "block") {
-            audiocontent.style.display = "none";
+        if (audiocontent.stylesheet.display === "block") {
+            audiocontent.stylesheet.display = "none";
         } else {
-            videocontent.style.display = "block";
+            audiocontent.stylesheet.display = "block";
         }
     });
 }
